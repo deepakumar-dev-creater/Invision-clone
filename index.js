443380edf35a1c291852892fbacbf13737d993d4
deepakumar-dev-creater <sean4deepak@gmail.com>
2021-10-15 09:41:33 +0530

    add proper animation

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.to('.icons',{
-    scrollTrigger:{  
+gsap.to(".owner-title", {
+    scrollTrigger: {
+      trigger: ".icons",
+      scrub: true,
+      start: "-200 bottom",
+      end: "200 250",
+  
+      toggleActions: "play pause reverse pause",
+    },
+     y:220,
+    duration: 3,
+  });
+gsap.to(".icons", {
+  scrollTrigger: {
+    trigger: ".icons",
+    scrub: true,
+    start: " bottom",
+    end: "200 250",
 
-        trigger:'.icons',
-        toggleActions:'play pause reverse pause'   },
-    y:300,
-    duration:3,
-
-})
+    toggleActions: "play pause reverse pause",
+  },
+  x: 1200,
+  duration: 3,
+});

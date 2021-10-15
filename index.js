8466ac7928a3bc1d40e39187015717e1142be50f
Deepak Kumar <sean4deepak@gmail.com>
2021-10-15 13:40:27 +0530

    alignment
    Signed-off-by: Deepak Kumar <sean4deepak@gmail.com>

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.to(".owner-title", {
     scrollTrigger: {
-      trigger: ".icons",
-      scrub: true,
-      start: "-200 bottom",
-      end: "200 250",
-  
-      toggleActions: "play pause reverse pause",
-    },
-     y:220,
-    duration: 3,
-  });
-gsap.to(".icons", {
-  scrollTrigger: {
-    trigger: ".icons",
-    scrub: true,
-    start: " bottom",
-    end: "200 250",
+        trigger: ".icons",
+        scrub: true,
+        start: "-200 bottom",
+        end: "200 250",
 
-    toggleActions: "play pause reverse pause",
-  },
-  x: 1200,
-  duration: 3,
+        toggleActions: "play pause reverse pause",
+    },
+    y: 225,
+    duration: 3,
+});
+gsap.to(".icons", {
+    scrollTrigger: {
+        trigger: ".icons",
+        scrub: true,
+        start: " bottom",
+        end: "200 center",
+        toggleActions: "play pause reverse pause",
+    },
+    x: 1200,
+    duration: 3,
 });
